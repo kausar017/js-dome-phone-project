7b272dd1bcb70822e2174475453c6d11e686f385
kousra <mdkousarmia71@gmail.com>
2024-09-18 23:17:46 +0600

    home page disign

--- js/login.js
+++ js/login.js
@@ -4,5 +4,17 @@ document.getElementById('login-btn')
     .addEventListener('click', function (event) {
         event.preventDefault();
     console.log('login button clickd');
+
+    const phoneNumbar = document.getElementById('phone-numbar').value;
+    const pinNumbar = document.getElementById('pin-numbar').value;
+    console.log('phone-numbar:',phoneNumbar,);
+    console.log('pin-numbar:',pinNumbar,);
+    
+    if(phoneNumbar === '017' && pinNumbar === '1234'){
+        console.log('you are logind');
+        window.location.href = '/home.html';        
+    }else{
+        alert('entar your carect phone & pin numbar')
+    }
     
 })
