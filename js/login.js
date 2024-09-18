ce345a54d50b9daffbb0a1514f5dc83734659592
kousra <mdkousarmia71@gmail.com>
2024-09-18 22:36:02 +0600

    ksdjfklas

---
+++ js/login.js
@@ -0,0 +1,8 @@
+
+//document.getElementBYId('id').addEvenLisanar('click', finction)
+document.getElementById('login-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+    console.log('login button clickd');
+    
+})
