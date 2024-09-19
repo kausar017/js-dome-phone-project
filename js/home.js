c8dcdbc385023c338144bf7ed1dbb07ec83c5076
kousra <mdkousarmia71@gmail.com>
2024-09-19 15:05:57 +0600

    carant balans

--- js/home.js
+++ js/home.js
@@ -10,5 +10,21 @@ document.getElementById('btn-add-Money')
     const addPinInput = document.getElementById('add-pin-numbar').value;
         console.log(addPinInput);
         
+    if(addPinInput === '1234') {
+        console.log('adding your money to your account');
+        const balans = document.getElementById('account-balans').innerText;
+        console.log(balans);
+    
+    const addMoneyNumbar =  parseFloat(addMoneyInput);
+    const balansNumbar = parseFloat(balans);
+    const carentBalans = addMoneyNumbar + balansNumbar;
+    console.log(carentBalans);
+    
+    document.getElementById('account-balans').innerText = carentBalans;
+
+    }
+    else{
+        alert('Failed to add money')
+    }
         
-});
+}); 
