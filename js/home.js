346a2838dd3e8da773285df3956dac4756da4977
kousra <mdkousarmia71@gmail.com>
2024-09-19 08:12:36 +0600

    pin & amount

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
-console.log('klsk');
+//add money to the account
+
+document.getElementById('btn-add-Money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log('add money lesanar');
+
+    const addMoneyInput = document.getElementById('add-money-input').value;
+        console.log(addMoneyInput);
+    const addPinInput = document.getElementById('add-pin-numbar').value;
+        console.log(addPinInput);
+        
+        
+});
